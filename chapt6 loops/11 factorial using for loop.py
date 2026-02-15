296972b443b07c840b9fb7339e4640228706259a
Nandinikale23 <nandinikale0623@gmail.com>
2026-02-15 11:02:39 +0530

    Delete 11 factorial using for loop.py

--- chapt6 loops/11 factorial using for loop.py
+++
@@ -1,8 +0,0 @@
-#factorial of 5 is 5!=1*2*3*4*5
-
-n= int(input("enter number to calculate factorial:"))
-product=1
-for i in range(1,n+1):
-    product = product * i
-    
-print(f"the factorial {n} is {product}")
