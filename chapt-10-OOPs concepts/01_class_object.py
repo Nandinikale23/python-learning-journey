92e22f62303c233cbc1c81bfd169212efa3a204a
Nandinikale23 <nandinikale0623@gmail.com>
2026-03-14 09:45:44 +0530

    Demonstrate class attributes and instance attributes using Employee class

---
+++ chapt-10-OOPs concepts/01_class_object.py
@@ -0,0 +1,17 @@
+class Employee:
+    lang = "py" # this is class attributes
+    salary= 200000
+    
+
+em1= Employee() # create obj 
+em1.name="nandini" # this is an instance attribute
+print(em1.name,"\n", em1.lang,"\n", em1.salary)
+
+
+
+em2 =Employee()
+em2.name="rudra"  # this is an instance attribute
+print(em2.name, "\n", em2.salary, "\n", em2.lang)
+
+
+
