18d46c4f8319b15fd83efb61195eb6d272cffb77
Nandinikale23 <nandinikale0623@gmail.com>
2026-04-01 18:49:59 +0530

    implement logical operator

---
+++ chapt-02- operators/03_logical_operator.py
@@ -0,0 +1,10 @@
+#it is used with condition
+''' 
+| Operator | Meaning      |
+| -------- | ------------ |
+| and      | both true    |
+| or       | any one true |
+| not      | reverse      |
+
+'''
+print(True and False)
