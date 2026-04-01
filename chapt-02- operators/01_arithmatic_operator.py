4f0efadef2dac2ef4f8813f67f802fe5537689d8
Nandinikale23 <nandinikale0623@gmail.com>
2026-04-01 18:49:13 +0530

    implement arithmatic program

---
+++ chapt-02- operators/01_arithmatic_operator.py
@@ -0,0 +1,20 @@
+'''| Operator      | Example |
+| --------         | ------- |
+| +                | a + b   |
+| -                | a - b   |
+| *                | a * b   |
+| /:division       | a / b   |
+|% : modulus       | a % b   |
+|//:floor division | a // b  |
+|** : exponent     | a ** b  |
+
+/ → normal divide
+% → remainder
+// → no decimal, only int
+** → for square,cube 
+'''
+
+a = 10
+b = 3
+print(a + b)
+print(a % b)
