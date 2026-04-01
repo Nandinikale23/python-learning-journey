33ef18e6ec972effbee5c0d853b8eddcb5368765
Nandinikale23 <nandinikale0623@gmail.com>
2026-04-01 18:51:36 +0530

    Delete problem3.py

--- chapt-03- problemset/problem3.py
+++
@@ -1,8 +0,0 @@
-#WAP to detect double space in a string
-#find()method is used for find spaces of any charater in string 
-#find()method show -1 means no space or shows any index means yes 
-
-name="helllow everyone!   how are you"
-print(name.find("  "))
-
-print(name.find("how"))
