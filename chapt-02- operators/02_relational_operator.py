81a722442452b9175aa47c460e9bb85f8ef5f604
Nandinikale23 <nandinikale0623@gmail.com>
2026-04-01 18:49:33 +0530

    implement relation operator

---
+++ chapt-02- operators/02_relational_operator.py
@@ -0,0 +1,11 @@
+''' It is use for Compare values (result is True/False)
+
+| Operator | Example |
+| -------- | ------- |
+| ==       | a == b  |
+| !=       | a != b  |
+| >        | a > b   |
+| <        | a < b   |
+'''
+
+print(10 > 5)
