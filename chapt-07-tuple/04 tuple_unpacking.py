c1cf41b54db280968ab99ccf0e32024f001fa513
Nandinikale23 <nandinikale0623@gmail.com>
2026-04-08 07:43:27 +0530

    Create 04 tuple_unpacking.py

---
+++ chapt-07-tuple/04 tuple_unpacking.py
@@ -0,0 +1,13 @@
+#tuple unpacking:=  means taking value from tuple and storing them into variables
+
+#imp rule:= number of variables= number of values
+
+''' t=(1,2,3)
+    a,b= t    # error '''
+    
+
+t=(10,20,30)
+a,b,c = t
+print(a)
+print(b)
+print(c)
