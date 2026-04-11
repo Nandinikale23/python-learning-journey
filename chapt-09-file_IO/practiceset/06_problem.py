122e2a501d9d1c4e2c221d6b950a7a750ee67eeb
Nandinikale23 <nandinikale0623@gmail.com>
2026-04-11 08:58:00 +0530

    Delete 06_problem.py

--- chapt-09-file_IO/practiceset/06_problem.py
+++
@@ -1,9 +0,0 @@
-''' WAP to mine a file and find out whether it contains "python" '''
-
-with open("pb_06file.txt") as f:
-    content = f.read()
-    
-if("python" in content):
-    print("yes python is present")
-else:
-    print("no python is not present ")
