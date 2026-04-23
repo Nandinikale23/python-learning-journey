8048be86540a4c5163e2afd0faf8bace942cc2eb
Nandinikale23 <nandinikale0623@gmail.com>
2026-04-23 07:28:47 +0530

    not imp removed

--- chapt-11 -Advance python/02_type hint.py
+++
@@ -1,8 +0,0 @@
-'''type hint(:)= is used to define data types of variables'''
-
-n : int= 10
-
-name : str ="nandini"
-
-def sum(a:int, b:int) -> int:
-    return a+b
--- chapt-11 -Advance python/05_main.py
+++
@@ -1,6 +0,0 @@
-from module import myfunc
-
-if __name__=="__main__":
-    #if this code is directly executed by running the file its present in
-    print("we are directly running this code ")
-    
