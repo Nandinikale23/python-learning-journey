757ba8aeb5daff7fcbdc3f718a6c49b2ec725d0b
Nandinikale23 <nandinikale0623@gmail.com>
2026-04-24 16:57:16 +0530

    Create 03_default_argument.py

---
+++ chapt-11-functions & recusions/03_default_argument.py
@@ -0,0 +1,8 @@
+#already value set in function, it is used when no value is given
+
+def goodday(name, ending="thank you"):
+    print(f"good day ,{name}")
+    print(ending)
+    
+goodday("nandini","thanks")
+goodday("rudra")
