5afdb8c0d98d45dc87aa096830eb2a577bba0b79
Nandinikale23 <nandinikale0623@gmail.com>
2026-04-24 16:57:13 +0530

    Create 02_fun_with_arguments.py

---
+++ chapt-11-functions & recusions/02_fun_with_arguments.py
@@ -0,0 +1,12 @@
+
+#Function with argument means a function that takes value from user (input).
+
+
+
+def goodday(name,ending):
+    print("good day,"+name)
+    print(ending)
+    
+goodday("nandini","thank you")
+goodday("rudra","thank you")
+goodday("ranvir","thanks")
