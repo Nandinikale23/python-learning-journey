59be5722b75bb6827f34b2d2b6107e470b3d6cb4
Nandinikale23 <nandinikale0623@gmail.com>
2026-04-28 06:36:41 +0530

    pyhton program to implement multiplication of number

---
+++ chapt-11-functions & recusions/practiceset chapt8/problem_07.py
@@ -0,0 +1,10 @@
+#WAP python function to print multiplication table of given number
+
+def table(n):
+    for i in range(1,11):
+        print(f"{n} * {i} = {n*i}")
+        
+table(5)
+
+    
+    
