30b0ada188e31c7bebb59233faabb049e5c46579
Nandinikale23 <nandinikale0623@gmail.com>
2026-05-07 20:51:39 +0530

    Create 03_property_decorator.py

---
+++ chapt-11-OOPs concepts/decorator/03_property_decorator.py
@@ -0,0 +1,24 @@
+#property decorator:= It allows a method to behave like a variable
+
+
+class Employee:
+    
+    @property
+    def name(self):
+        return f"{self.fname} {self.lname} {self.mname} "
+    
+    @name.setter
+    def name(self,value):
+        self.fname=value.split(" ")[0]
+        self.mname=value.split(" ")[1]
+        self.lname=value.split(" ")[2]
+        
+    
+e= Employee()
+e.name ="nandini santosh kale"
+print(e.fname, e.mname, e.lname)
+
+
+
+    
+    
