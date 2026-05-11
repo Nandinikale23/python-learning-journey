89aea54c069786db470ccddd27a595c38561131a
Nandini kale <nandinikale0623@gmail.com>
2026-05-11 06:48:32 +0530

    Delete 01_try_catch.py

--- chapt-12-Exception_handling/01_try_catch.py
+++
@@ -1,9 +0,0 @@
-try:       #risky code
-    a= int(input("hey, enter a number:"))
-    print(a)
-    
-except Exception as e:              #error handling
-    print(e)
-    
-print("thank you")  #after exception handling program not stop so print "thank you"
-    
