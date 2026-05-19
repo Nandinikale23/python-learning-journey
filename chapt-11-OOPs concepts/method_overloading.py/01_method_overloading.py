53b40a154996b6bae14fe44d94f172282ae68e24
Nandini kale <nandinikale0623@gmail.com>
2026-05-19 12:29:08 +0530

    implement method overloading program

---
+++ chapt-11-OOPs concepts/method_overloading.py/01_method_overloading.py
@@ -0,0 +1,5 @@
+'''python does not support method overloading because if we create same method again then old method gets replaced and create confusion'''
+''' method overloading means create multiple methods with same name but different parameters'''
+
+
+#in python uses the default argument and *args 
