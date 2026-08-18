610309f6e9def13f368ebcd98ecd5680cc154a71
Nandini kale <nandinikale0623@gmail.com>
2026-08-18 17:04:40 +0530

    Create program_01.py

---
+++ Practice programs/01_numbers and operators program/program_01.py
@@ -0,0 +1,8 @@
+#Q.Take two numbers and print: Addition,Subtraction,Multiplication,Division
+
+num1=int(input("enter num1: "))
+num2= int(input("enter num2: "))
+print(f"addition of num1 and num2 is {num1+ num2}")
+print(f"sub of num1 and num2 is {num1-num2}")
+print(f"mul of num1 and num2 is {num1*num2}")
+print(f"div of num1 and num2 is {num1/num2}")
