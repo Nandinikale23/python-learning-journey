a2ca313f529dd7e7a28ffbe57ebfb220266baa29
Nandini kale <nandinikale0623@gmail.com>
2026-09-10 23:21:30 +0530

    Create program_02.py

---
+++ Practice programs/02_conditions(if-else)/program_02.py
@@ -0,0 +1,9 @@
+#check number is positive ,negative, or zero 
+
+n=int(input("enter the number: "))
+if n>0:
+    print("number is positive")
+elif n<0:
+    print("number is negative")
+elif n==0:
+    print("number is zero ")
